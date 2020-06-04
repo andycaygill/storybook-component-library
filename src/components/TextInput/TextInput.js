f77cc39c5c1d24fadb03656d6d44b8970802f4dd
Andrew Caygill <andy8730@googlemail.com>
2020-06-04 09:41:48 -0700

    Updating styling for form input component and adding invalid state

--- src/components/TextInput/TextInput.js
+++ src/components/TextInput/TextInput.js
@@ -2,68 +2,36 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
+
+const InputContainer = styled.div`
+    margin: 0 0 20px 0;
+`;
+
+
 const StyledInput = styled.input`
-    font-size: 1.4rem;
+    font-size: 1.6rem;
     background-color: var(--neutral-200);
     border: 0;
     padding: 10px;
     color: #000;
+    margin: 0 0 5px 0;
 `;
 
-/*
-const StyledButton = styled.button`
-    font-family: 'Montserrat', sans-serif;
-    border-radius: 25px;
+const StyledLabel = styled.label`
+    display: block;
     font-size: 1.4rem;
-    color: #fff;
-    text-align: center;
-    font-weight: 600;
-    display: inline-block;
-    padding: 10px 25px;
-    margin: 0 3px;
-    transition: 0.2s ease;
-    border: 0;
-    cursor: pointer;
-    line-height: 1;
-    background-color: var(--aqua);
+    margin: 0 0 5px 0;
 
-    &:hover, &:focus{
-        background-color: var(--aqua-dark);
-        color: #ffffff;
-    }
-    &:focus{
-        box-shadow: inset 0 0 0px 3px #fff, 0 0 0 2px var(--aqua-dark);
-        outline: none;
-    }
-    &:active{
-        background-color: var(--aqua-light);
-    }
-
-    ${props => props.appearance === APPEARANCES.SECONDARY && `
-        background-color: #ffffff;
-        border: 1px solid;
-        color: var(--aqua);
-    `}
-
-    ${props => props.appearance === APPEARANCES.TERTIARY && `
-        background-color: #ffffff;
-        color: var(--aqua);
-    `}
-
-    ${props => props.disabled && `
-        cursor: not-allowed !important;
-        background-color: var(--neutral-400);
-        color: #fff;
-        border: none;
-
-        &:hover, &:focus, &:active {
-            background-color: var(--neutral-400);
-            box-shadow: none;
-            color: #fff;
-        }
+    ${props => props.invalid && `
+        color: red;
     `}
 `;
-*/
+
+const InvalidLabel = styled.span`
+    color: var(--error-100);
+    display: block;
+    font-size: 1.4rem;
+`;
 
 export function TextInput( {
         labelText,
@@ -74,12 +42,15 @@ export function TextInput( {
         ...props
     }){
     return(
-        <div className="form-field">
-            <label for={id}>
+        <InputContainer>
+            <StyledLabel for={id} invalid={invalid}>
                 {labelText}
-            </label>
-            <StyledInput id={id} type={type} {...props} />          
-        </div>
+            </StyledLabel>
+            <StyledInput aria-invalid={invalid} id={id} type={type} {...props} />
+            {invalid && invalidText && (
+                <InvalidLabel>{invalidText}</InvalidLabel>
+            )}       
+        </InputContainer>
     );
 }
 
--- src/components/TextInput/TextInput.stories.js
+++ src/components/TextInput/TextInput.stories.js
@@ -7,6 +7,12 @@ export default { title: 'Text Input' };
 
 export const Input = () => (
     <>
-        <TextInput type="text" id="form-field-1" placeholder="Enter Text" />
+        <TextInput type="text" id="form-field-1" labelText="First name" placeholder="Enter Text" />
+    </>
+);
+
+export const Validation = () => (
+    <>
+        <TextInput type="text" invalid="true" labelText="First name" invalidText="Please enter a valid name" id="form-field-2" placeholder="Enter Text" />
     </>
 );
